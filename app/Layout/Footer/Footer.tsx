c6910206fc4d1de1e0fbbf3acd61344a67eb04b3
Mart95Dev <d.wehrling@gmail.com>
2024-06-20 12:00:18 +0200

    Create components Header and Footer. Install library React-icons

---
+++ app/Layout/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Footer() {
+  return (
+    <div>Tous droits réservés : github - contact</div>
+  )
+}
---
+++ app/Layout/Header/Header.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Image de Géolocalisation</h1>
+    </div>
+  );
+}
