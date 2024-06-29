f3eaf7d173ed4d5b27e26304b91f3a2551992d17
Mart95Dev <d.wehrling@gmail.com>
2024-06-29 13:23:56 +0200

    Add styling BoxSearchClimatCountryInformation

--- app/components/BoxSearchClimatCountryInformation/BoxSearchClimatCountryInformation.tsx
+++ app/components/BoxSearchClimatCountryInformation/BoxSearchClimatCountryInformation.tsx
@@ -3,8 +3,8 @@
 export default function BoxSearchClimatCountryInformation() {
   return (
     <div className="search-box">
-<input type="search"className="input-search lato-regular" placeholder="Ville ou Pays"/>
-<input type="button" className="button-search lato-regular" value="Rechercher"  />
+<input type="search" className="input-search lato-regular" placeholder="Ville ou Pays"/>
+<input type="button" className="button-search lato-bold" value="Rechercher"  />
     </div>
   )
 }
