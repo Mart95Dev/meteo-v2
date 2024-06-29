d70b9868c84cef1d6d34210de05015a12b46cd84
Mart95Dev <d.wehrling@gmail.com>
2024-06-29 12:51:54 +0200

    update styling BoxClimatCountryInformation

--- app/reusable/BoxClimatCountryInformation/BoxClimatCountryInformation.tsx
+++ app/reusable/BoxClimatCountryInformation/BoxClimatCountryInformation.tsx
@@ -2,19 +2,19 @@ export default function BoxClimatCountryInformation() {
   return (
     <div className="weather-box">
       <div className="country-flag poppins-regular ">drapeau</div>
-      <div className="country-name poppins-regular ">
-        <span>Pays:</span>
-        <span>Royaume-uni</span>
+      <div className="country-name poppins-semibold ">
+        
+        <span>Royaume-uni rrr</span>
       </div>
-      <div className="capital-name poppins-regular ">
-        <span>Capitale:</span>
+      <div className="capital-name poppins-semibold ">
+        
         <span>Andorre-la-veille</span>
       </div>
 
-      <div className="weather-type">type de temps</div>
+      <div className="weather-type poppins-semibold">type de temps</div>
       <div className="weather-icon">icon</div>
-      <div className="temperature-label poppins-regular">
-        <span>Température:</span>
+      <div className="temperature-label poppins-semibold">
+        <span>Température</span>
         
       </div>
       <div className="lato-regular real-feel">
@@ -25,7 +25,7 @@ export default function BoxClimatCountryInformation() {
         <span>Ressenti:</span>
         
       </div>
-      <div className="climate-label poppins-regular">Climat</div>
+      <div className="climate-label poppins-semibold">Climat</div>
       <div className="rain lato-regular">
         <span>Pluie:</span>
         
