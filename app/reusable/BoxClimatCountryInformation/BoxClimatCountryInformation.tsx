36ba705a82bdbdca242861c8cc102f0f4a5328d6
Mart95Dev <d.wehrling@gmail.com>
2024-06-29 11:36:52 +0200

    Add grid styling BoxClimatCountryInformation

--- app/reusable/BoxClimatCountryInformation/BoxClimatCountryInformation.tsx
+++ app/reusable/BoxClimatCountryInformation/BoxClimatCountryInformation.tsx
@@ -2,27 +2,41 @@ export default function BoxClimatCountryInformation() {
   return (
     <div className="weather-box">
       <div className="country-flag poppins-regular ">drapeau</div>
-      <div className="country-name poppins-regular ">Pays :</div>
-      <div className="capital-name poppins-regular ">Capitale :</div>
+      <div className="country-name poppins-regular ">
+        <span>Pays:</span>
+        <span>Royaume-uni</span>
+      </div>
+      <div className="capital-name poppins-regular ">
+        <span>Capitale:</span>
+        <span>Andorre-la-veille</span>
+      </div>
 
       <div className="weather-type">type de temps</div>
       <div className="weather-icon">icon</div>
-      <div className="temperature-label poppins-regular">Température</div>
+      <div className="temperature-label poppins-regular">
+        <span>Température:</span>
+        
+      </div>
       <div className="lato-regular real-feel">
         <span>Réelle :</span>
+        
       </div>
       <div className="feels-like lato-regular">
-        <span>Ressenti :</span>
+        <span>Ressenti:</span>
+        
       </div>
       <div className="climate-label poppins-regular">Climat</div>
       <div className="rain lato-regular">
-        <span>Pluie :</span>
+        <span>Pluie:</span>
+        
       </div>
       <div className="wind lato-regular">
-        <span>Vent :</span>
+        <span>Vent:</span>
+        
       </div>
       <div className="humidity lato-regular">
-        <span>Humidité :</span>
+        <span>Humidité:</span>
+       
       </div>
     </div>
   );
