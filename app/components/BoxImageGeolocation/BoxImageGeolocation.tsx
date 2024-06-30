f704b2c39403b1cb2e8c3f63cba4801e26ba1365
Mart95Dev <d.wehrling@gmail.com>
2024-06-30 19:13:48 +0200

    update component BoxImageGeolocation to typescript

--- app/components/BoxImageGeolocation/BoxImageGeolocation.tsx
+++ app/components/BoxImageGeolocation/BoxImageGeolocation.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
-import type { StaticImageData } from 'next/image'; 
+import type { StaticImageData } from "next/image";
 
-
-type itemImageProps ={
+type itemImageProps = {
   itemImage: StaticImageData;
-}
+};
 
-export default function BoxImageGeolocation({itemImage}: itemImageProps) {
+export default function BoxImageGeolocation({ itemImage }: itemImageProps) {
   return (
-    <Image
-    src={itemImage}
-    alt="Picture of the author"
-    width={360} 
-    height={175} 
-    // blurDataURL="data:..." automatically provided
-    // placeholder="blur" // Optional blur-up while loading
-  />  )
+    <div className="image-location-container">
+      <Image src={itemImage} alt="Picture of the author"  fill sizes="(max-width: 440px) 100vw, 440px"/>
+    </div>
+  );
 }
