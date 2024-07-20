538b15c8a538cc1901e47b04a399d45a2316f491
Mart95Dev <d.wehrling@gmail.com>
2024-07-20 17:06:48 +0200

    update code with useFlagStore and geolocationStore

--- app/page.tsx
+++ app/page.tsx
@@ -11,43 +11,37 @@ import CategoryTitle from "./reusable/CategoryTitle/CategoryTitle";
 import { TbMapSearch } from "react-icons/tb";
 import { IoLocation } from "react-icons/io5";
 import Aside from "./Layout/Aside/Aside";
-// import Data from "./data/languages.json"
+import CodeFlags from "./data/codeFlags.json";
+import Flag from "./components/Flag/Flag";
+import useFlagStore from "./store/useFlagStore";
 
 export default function Home() {
   //state
-  const[error, setError]= useState('');
+  const [error, setError] = useState("");
+
   const {
     latitude,
     longitude,
-    language,
-    flag,
+    language_browser,
     country,
     country_code,
     city,
-    icon,
-    temp_feel,
-    temp_real,
-    rain,
-    wind,
-    humidity,
     setCoordinates,
-    setLanguage,
-    setFlag,
+    setlanguageBrowser,
     setCity,
     setCountry,
-    setCountry_code, 
+    setCountry_code,
     setIsGeolocationEnabled,
-    setIcon,
-    setTempFeel,
-    setTempReal,
-    setRain,
-    setWind,
-    setHumidity
   } = useGeolocationStore();
+
+  const {
+    flag_geolocalisation,
+    setFlagGeolocalisation,
+    setNoFlagGeolocalisation,
+  } = useFlagStore();
   const { width } = useWindowSize();
   const showAside = width >= 701; // Afficher l'aside si la largeur est >=
   const apiKey = process.env.NEXT_PUBLIC_API_KEY_OPENWEATHER;
-   
 
   useEffect(() => {
     const browserLanguage = window.navigator.language.slice(0, 2);
@@ -57,28 +51,35 @@ export default function Home() {
         setCoordinates(position.coords.latitude, position.coords.longitude);
       },
       async (error) => {
-        console.error("La géolocalisation est désactivée dans votre navigateur. Nous utiliserons votre adresse IP pour estimer votre position et vous fournir une prévision météo.");
-        setIsGeolocationEnabled(false); // Géolocalisation échouée
-  
-        try {
-          const response = await fetch('https://api.ipify.org?format=json');
-          const data = await response.json();
-          const ipAddress = data.ip;
-  
-          const locationIP = await fetch(`http://ip-api.com/json/${ipAddress}`);
-          const locationIpResponse = await locationIP.json()
-          
-          setCity(locationIpResponse.city);
-          setCountry(locationIpResponse.country);
-        } catch (ipError) {
-          console.error('Erreur lors de la récupération de l\'adresse IP :', ipError);
-          setError('Impossible de récupérer les données de localisation par votre Ip ou la localisation via votre naviagateur.'); 
-        }
-      },
-    );
-    
+        const errorMessage =
+          "La géolocalisation est désactivée dans votre navigateur. Nous utiliserons votre adresse IP pour estimer votre position et vous fournir une prévision météo.";
+        setError(errorMessage);
+        setIsGeolocationEnabled(false); // Géolocalisation navigateur non autorisé
 
-    setLanguage(browserLanguage == "fr" ? browserLanguage : "en");
+        try {
+          const response = await fetch("https://api.ipify.org?format=json");
+          const data = await response.json();
+          // const ipAddress = data.ip;
+
+          const locationIP = await fetch(`http://ip-api.com/json/${data.ip}`);
+          const locationIpResponseJson = await locationIP.json();
+
+          setCity(locationIpResponseJson.city);
+          setCountry(locationIpResponseJson.country);
+          setCountry_code(locationIpResponseJson.countryCode.toUpperCase());
+        } catch (ipError) {
+          console.error(
+            "Erreur lors de la récupération de l'adresse IP :",
+            ipError
+          );
+          setError(
+            "Impossible de récupérer les données de localisation par votre Ip ou la localisation via votre naviagateur."
+          );
+        }
+      }
+    );
+
+    setlanguageBrowser(browserLanguage == "fr" ? browserLanguage : "en");
 
     const fechtCountryWithLatitudeAndLongitude = async (
       latitude: number,
@@ -86,7 +87,7 @@ export default function Home() {
       language: string
     ) => {
       try {
-            const response = await fetch(
+        const response = await fetch(
           `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=${language}`
         );
 
@@ -98,7 +99,6 @@ export default function Home() {
 
         const data = await response.json();
         console.log(data);
-        
 
         // Extraire le nom de la ville de la réponse de l'API
         // const cityName = data[0]?.name || "Ville inconnue"; // Gérer le cas où la ville n'est pas trouvée
@@ -106,7 +106,6 @@ export default function Home() {
         //  setCity(cityName);
         //  setCountry_code(country_code);
         //  return;
-
       } catch (error) {
         console.error(
           "Erreur lors de la récupération des données de la ville :",
@@ -116,26 +115,42 @@ export default function Home() {
       }
     };
 
-    const fetchWeatherData = async () => {
-      try {
-        // ... (votre logique de récupération de données météo en utilisant city, language et apiKey)
-      } catch (error) {
-        console.error('Erreur lors de la récupération des données météo :', error);
-        setError('Impossible de récupérer les données météo.');
-      }
-    };
-    if (latitude !== null && longitude !== null && language && apiKey) {
-      fechtCountryWithLatitudeAndLongitude(latitude, longitude, language);
+    if (latitude !== null && longitude !== null && language_browser && apiKey) {
+      fechtCountryWithLatitudeAndLongitude(latitude, longitude, language_browser);
     }
-  }, [setCoordinates, setLanguage,setCity, setCountry,setCountry_code, language, latitude, longitude, apiKey]);
+  }, [
+    setCoordinates,
+    setlanguageBrowser,
+    setCity,
+    setCountry,
+    setCountry_code,
+    setIsGeolocationEnabled,
+    language_browser,
+    latitude,
+    longitude,
+    apiKey,
+  ]);
 
+  ////////////
+  useEffect(() => {
+    // Logique du drapeau ici
+    const testCodesFlags: boolean =
+      CodeFlags.codes_flags.includes(country_code);
+    if (testCodesFlags) {
+      setFlagGeolocalisation(`https://countryflagsapi.netlify.app/flag/${country_code}.svg`);
+    } else {
+      setNoFlagGeolocalisation(Flag);
+    }
+  }, [country_code, setFlagGeolocalisation, setNoFlagGeolocalisation]); // Dépend uniquement de country_code
+
+  ////////////
   ///////////////
   useEffect(() => {
     console.log(language);
     console.log(country);
     console.log(city);
     console.log(country_code);
-
+    console.log(flag_geolocalisation);
   });
   //////////////
 
