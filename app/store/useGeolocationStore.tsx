5ab35063e22716a11abce909489a4d2d885e8ca1
Mart95Dev <d.wehrling@gmail.com>
2024-07-20 17:05:34 +0200

    update geolocalisationStore and delete flag elements

--- app/store/useGeolocationStore.tsx
+++ app/store/useGeolocationStore.tsx
@@ -1,64 +1,53 @@
-import {create} from 'zustand';
+import { create } from "zustand";
 
-interface GeolocationState {
-  latitude: number | null;
-  longitude: number | null;
-  language: string | null;
-  isGeolocationEnabled: boolean;
-  flag: string;
-  country_code: string | null;
-  country: string | null;
-  city: string | null;
+interface localisationWeatherData {
+  temperature: number;
   icon: string;
   temp_real: number | null;
   temp_feel: number | null;
   rain: number | null;
   wind: number | null;
   humidity: number | null;
+}
+
+interface GeolocationState {
+  latitude: number | null;
+  longitude: number | null;
+  language_browser: string | null;
+  isGeolocationEnabled: boolean;
+  country_code: string;
+  country: string | null;
+  city: string | null;
+  localisationWeather: localisationWeatherData | null;
   setCoordinates: (latitude: number, longitude: number) => void;
-  setLanguage: (language: string) => void;
-  setIsGeolocationEnabled: (isGeolocationEnabled: boolean)=> void;
-  setFlag: (flag: string) => void;
+  setlanguageBrowser: (language_broswer: string) => void;
+  setIsGeolocationEnabled: (isGeolocationEnabled: boolean) => void;
+
   setCountry: (country: string) => void;
-  setCountry_code: (country_code: string)=> void;
+  setCountry_code: (country_code: string) => void;
   setCity: (city: string) => void;
-  setIcon: (icon: string) => void;
-  setTempReal: (temp_real: number) => void;
-  setTempFeel: (temp_feel: number) => void;
-  setRain: (rain: number) => void;
-  setWind: (wind: number) => void;
-  setHumidity: (humidity: number) => void;
 }
 
 const useGeolocationStore = create<GeolocationState>((set) => ({
+  localisationWeather: null,
   latitude: null,
   longitude: null,
   language: null,
   isGeolocationEnabled: true,
-  flag: "",
   country: null,
-  country_code: null,
+  country_code: "",
   city: null,
-  icon: "",
-  temp_real: null,
-  temp_feel: null,
-  rain: null,
-  wind: null,
-  humidity: null,
+  flag: "",
+  no_flag: Flag,
   setCoordinates: (latitude, longitude) => set({ latitude, longitude }),
-  setLanguage: (language)=> set({language}),
-  setIsGeolocationEnabled: (isGeolocationEnabled)=>set({isGeolocationEnabled}),
-  setFlag: (flag) => set({ flag }),
+  setLanguage: (language) => set({ language }),
+  setIsGeolocationEnabled: (isGeolocationEnabled) =>
+    set({ isGeolocationEnabled }),
   setCountry: (country) => set({ country }),
+  setCountry_code: (country_code) => set({ country_code }),
   setCity: (city) => set({ city }),
-  setIcon: (icon) => set({ icon }),
-  setTempReal: (temp_real) => set({ temp_real }),
-  setTempFeel: (temp_feel) => set({ temp_feel }),
-  setRain: (rain) => set({ rain }),
-  setWind: (wind) => set({ wind }),
-  setHumidity: (humidity) => set({ humidity }),
-  setCountry_code: (country_code)=> set({country_code})
+  setlocalisationWeather: (data: localisationWeatherData) =>
+    set({ localisationWeather: data }),
 }));
 
 export default useGeolocationStore;
-
