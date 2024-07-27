9a1e869ad7521cec985d70dd751a6e7b89f2270b
Mart95Dev <d.wehrling@gmail.com>
2024-07-27 12:37:45 +0200

    Delete elements and update file

--- app/page.tsx
+++ app/page.tsx
@@ -16,25 +16,28 @@ import CategoryTitle from "./reusable/CategoryTitle/CategoryTitle";
 import { TbMapSearch } from "react-icons/tb";
 import { IoLocation } from "react-icons/io5";
 import Aside from "./Layout/Aside/Aside";
-
-import codeCountiresFiltered from "./data/filtered_curiexplore-pays.json";
+import useErrorStore from "./store/useErrorStore";
 
 export default function Home() {
   //state
-  const [error, setError] = useState("");
 
-  const { latitude, longitude, setCoordinates, setIsGeolocationEnabled,locationWeather } =
-    useGeolocationStore();  
+  const { setError } = useErrorStore();
+  const {
+    latitude,
+    longitude,
+    setCoordinates,
+    setIsGeolocationEnabled,
+    locationWeather,
+  } = useGeolocationStore();
 
   const { language_browser, setlanguageBrowser } = useLanguageBrowserStore();
 
-  const {geo_city,geo_country} = useCountryStore();
-  const {flag_geolocation, no_flag_geolocation} = useFlagStore();
+  const { geo_city, geo_country } = useCountryStore();
+  const { flag_geolocation, no_flag_geolocation } = useFlagStore();
 
-   const { width } = useWindowSize();
+  const { width } = useWindowSize();
   const showAside = width >= 701; // Afficher l'aside si la largeur est >=
-  // const apiKey = process.env.NEXT_PUBLIC_API_KEY_OPENWEATHER;
-
+ 
   useEffect(() => {
     const browserLanguage = window.navigator.language.slice(0, 2);
 
@@ -75,73 +78,32 @@ export default function Home() {
 
     setlanguageBrowser(browserLanguage == "fr" ? browserLanguage : "en");
 
-    // const fechtWeatherWithLatitudeAndLongitude = async (
-    //   latitude: number,
-    //   longitude: number,
-    //   language: string
-    // ) => {
-    //   try {
-    //     const response = await fetch(
-    //       `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=${language}`
-    //     );
-
-    //     if (!response.ok) {
-    //       throw new Error(
-    //         "Erreur lors de la récupération des données de la ville."
-    //       );
-    //     }
-
-    //     const data = await response.json();
-    //     console.log(data);
-
-    //     // const responseWeatherData = {
-    //     //   temperature: ,
-    //     //   icon:,
-    //     //   temp_real: ,
-    //     //   temp_feel: ,
-    //     //   rain: ,
-    //     //   wind: ,
-    //     //   humidity: ,
-    //     // }
-
-    //     // Extraire le nom de la ville de la réponse de l'API
-    //     // const cityName = data[0]?.name || "Ville inconnue"; // Gérer le cas où la ville n'est pas trouvée
-    //     // const country_code = data[0]?.country || "code inconnu";
-    //     //  setCity(cityName);
-    //     //  setCountry_code(country_code);
-    //     //  return;
-    //   } catch (error) {
-    //     console.error(
-    //       "Erreur lors de la récupération des données de la ville :",
-    //       error
-    //     );
-    //     throw error; // Propager l'erreur pour la gestion dans useEffect
-    //   }
-    // };
-
     if (latitude !== null && longitude !== null && language_browser) {
       weatherWithLatitudeAndLongitude(latitude, longitude, language_browser);
     }
-
-  
   }, [
     setCoordinates,
     setlanguageBrowser,
     setIsGeolocationEnabled,
+    setError,
     language_browser,
     latitude,
     longitude,
   ]);
 
-    useEffect(() => {
-console.log(geo_country);
-console.log(geo_city);
-console.log(flag_geolocation);
-console.log(no_flag_geolocation);  
-console.log(locationWeather);  
-   }, [geo_country,geo_city,flag_geolocation,no_flag_geolocation,locationWeather ]); 
-
-  
+  useEffect(() => {
+    console.log(geo_country);
+    console.log(geo_city);
+    console.log(flag_geolocation);
+    console.log(no_flag_geolocation);
+    console.log(locationWeather);
+  }, [
+    geo_country,
+    geo_city,
+    flag_geolocation,
+    no_flag_geolocation,
+    locationWeather,
+  ]);
 
   return (
     <>
