44dd6c4fbc27b0767e2b35414a87e40d5d3dba57
Mart95Dev <d.wehrling@gmail.com>
2024-07-28 11:15:26 +0200

    update Image NextConfig with openweather

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'http', // Le protocole utilisé par l'API OpenWeatherMap
+            hostname: 'openweathermap.org', // Le nom de domaine de l'API OpenWeatherMap
+            port: '',
+            pathname: '/img/wn/**', // Le chemin d'accès aux icônes météo
+          },
+        ],
+      },  
+};
 
 export default nextConfig;
