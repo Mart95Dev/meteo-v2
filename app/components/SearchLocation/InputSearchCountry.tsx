04123d13a85d0864728e8f7f66d6433e9abdcbaa
Mart95Dev <d.wehrling@gmail.com>
2024-07-28 09:41:58 +0200

    Rename component

---
+++ app/components/SearchLocation/InputSearchCountry.tsx
@@ -0,0 +1,10 @@
+
+
+export default function InputSearchClimatCountryInformation() {
+  return (
+    <div className="search-box">
+<input type="search" className="input-search" placeholder="Ville ou Pays"/>
+<input type="button" className="button-search lato-bold" value="Rechercher"  />
+    </div>
+  )
+}
