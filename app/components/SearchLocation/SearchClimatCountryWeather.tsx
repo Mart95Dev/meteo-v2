575d9cef4cafac9ae88da4c2c43fe6a02a72555d
Mart95Dev <d.wehrling@gmail.com>
2024-07-28 09:41:33 +0200

    Create component searchClimatCountryWeather

---
+++ app/components/SearchLocation/SearchClimatCountryWeather.tsx
@@ -0,0 +1,45 @@
+
+
+export default function SearchClimatCountryWeather() {
+  return (
+    <div className="weather-box">
+      <div className="country-flag poppins-regular ">Drapeau</div>
+      <div className="country-name poppins-semibold ">
+        
+        <span>Royaume-uni rrr</span>
+      </div>
+      <div className="capital-name poppins-semibold ">
+        
+        <span>Andorre-la-veille</span>
+      </div>
+
+      <div className="weather-type poppins-semibold">type de temps</div>
+      <div className="weather-icon">icon</div>
+      <div className="temperature-label poppins-semibold">
+        <span>Température</span>
+        
+      </div>
+      <div className="real-feel">
+        <span>Réelle :</span>
+        
+      </div>
+      <div className="feels-like">
+        <span>Ressenti:</span>
+        
+      </div>
+      <div className="climate-label poppins-semibold">Climat</div>
+      <div className="rain ">
+        <span>Pluie:</span>
+        
+      </div>
+      <div className="wind ">
+        <span>Vent:</span>
+        
+      </div>
+      <div className="humidity ">
+        <span>Humidité:</span>
+       
+      </div>
+    </div>
+  );
+}
