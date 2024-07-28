72f18329e739b58f5220c22b4e7ff5a6b685ef2f
Mart95Dev <d.wehrling@gmail.com>
2024-07-28 15:59:39 +0200

    Duplicate file

--- app/components/SearchLocation/SearchClimatCountryWeather.tsx
+++ app/components/SearchLocation/SearchClimatCountryWeather.tsx
@@ -1,45 +1,99 @@
+import useCountryStore from "@/app/store/useCountryStore";
+import useGeolocationStore from "@/app/store/useGeolocationStore";
+import useLanguageBrowserStore from "@/app/store/useLanguageBrowser";
+
+import FlagDisplay from "@/app/components/Flag/FlagDisplay";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+export default function GeolocationClimatCountryWeather() {
+  const [isLoading, setIsLoading]= useState(true)
+  const {geo_city, geo_country} = useCountryStore();
+  const {locationWeather}= useGeolocationStore();
+  const{language_browser}= useLanguageBrowserStore();
+  
+  
+    const altIcon =
+      language_browser == "fr"
+        ? `Description de l'icon météorologique de la journée: ${
+            locationWeather?.description
+          }`
+        : `Description of the day's weather icon: ${
+            locationWeather?.description
+          }`;
+  
+      const iconUrl = (typeof locationWeather?.icon === 'string') ? locationWeather.icon : '';
+
+useEffect(() => {
+  if(locationWeather){
+    return setIsLoading(false)
+  }
+}, [locationWeather])
 
 
-export default function SearchClimatCountryWeather() {
-  return (
-    <div className="weather-box">
-      <div className="country-flag poppins-regular ">Drapeau</div>
-      <div className="country-name poppins-semibold ">
-        
-        <span>Royaume-uni rrr</span>
-      </div>
-      <div className="capital-name poppins-semibold ">
-        
-        <span>Andorre-la-veille</span>
-      </div>
 
-      <div className="weather-type poppins-semibold">type de temps</div>
-      <div className="weather-icon">icon</div>
-      <div className="temperature-label poppins-semibold">
-        <span>Température</span>
-        
+         return (
+      <div className="weather-box">
+        <div className="country-flag poppins-regular ">
+          <FlagDisplay />
+        </div>
+        <div className="country-name poppins-semibold ">
+          <span>PAYS :</span>
+          <span className="weather-data">{geo_country.toUpperCase()}</span>
+        </div>
+        <div className="capital-name poppins-semibold ">
+          <span>VILLE :</span>
+          <span className="weather-data">{geo_city.toUpperCase()}</span>
+        </div>
+  
+        <div className="weather-type poppins-semibold">
+          {locationWeather?.description.toUpperCase()}
+        </div>
+        <div className="weather-icon">
+        {isLoading ? (
+          <p>Chargement...</p>
+        ) : ( // Afficher l'image si isLoading est false
+          <Image 
+            src={iconUrl}
+            alt={altIcon}
+            width={80}
+            height={80}                      
+          />
+        )}
+        </div>
+        <div className="temperature-label poppins-semibold">
+          <span>Température</span>
+        </div>
+        <div className="real-feel">
+          <span>
+            Réelle :
+            <span className="weather-data">
+              {locationWeather?.temp_real} °
+            </span>
+          </span>
+        </div>
+        <div className="feels-like">
+          <span>Ressenti: </span>
+          <span className="weather-data">
+            {locationWeather?.temp_feel} °
+          </span>
+        </div>
+        <div className="climate-label poppins-semibold">Climat</div>
+        <div className="rain ">
+          <span>Pluie:</span>
+          <span className="weather-data">{locationWeather?.rain}</span>
+        </div>
+        <div className="wind ">
+          <span>Vent:</span>
+          <span className="weather-data">{locationWeather?.wind} km/h</span>
+        </div>
+        <div className="humidity ">
+          <span>Humidité:</span>
+          <span className="weather-data">
+            {locationWeather?.humidity} %
+          </span>
+        </div>
       </div>
-      <div className="real-feel">
-        <span>Réelle :</span>
-        
-      </div>
-      <div className="feels-like">
-        <span>Ressenti:</span>
-        
-      </div>
-      <div className="climate-label poppins-semibold">Climat</div>
-      <div className="rain ">
-        <span>Pluie:</span>
-        
-      </div>
-      <div className="wind ">
-        <span>Vent:</span>
-        
-      </div>
-      <div className="humidity ">
-        <span>Humidité:</span>
-       
-      </div>
-    </div>
-  );
-}
+    );
+
+};
