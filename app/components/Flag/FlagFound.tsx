5c2533599deb65867dda378759f45b1e74aceaf1
Mart95Dev <d.wehrling@gmail.com>
2024-07-31 17:12:38 +0200

    Update display flag

--- app/components/Flag/FlagFound.tsx
+++ app/components/Flag/FlagFound.tsx
@@ -2,18 +2,40 @@ import Image from 'next/image'
 import useFlagStore from '@/app/store/useFlagStore'
 import useLanguageBrowserStore from '@/app/store/useLanguageBrowser'
 import useCountryStore from '@/app/store/useCountryStore'
+import { useEffect, useState } from 'react'
 
 
 export default function FlagFound() {
+  const [isLoading, setIsLoading] = useState(true);
+  const {flag_geolocation} = useFlagStore();
 
-    const altFlagImage = useLanguageBrowserStore.getState().language_browser == 'fr' ? `image du drapeau de ${useCountryStore.getState().geo_country}`: `Image of the flag of ${useCountryStore.getState().geo_country}`
+  useEffect(() => {
+    if(flag_geolocation){
+      return setIsLoading(false)
+    }
+    
+  }, [flag_geolocation])
+  
+       const altFlagImage = useLanguageBrowserStore.getState().language_browser == 'fr' ? `image du drapeau de ${useCountryStore.getState().geo_country}`: `Image of the flag of ${useCountryStore.getState().geo_country}`
 
   return (
-   <Image className='flag-border'
-   src={useFlagStore.getState().flag_geolocation}
-   alt={altFlagImage}
-   width={64}
-   height={35}
-   />
+    <div>
+      {isLoading ? <Image 
+        src="/images/icon_default.png"
+        alt="chargement en cours"
+        width={64}
+        height={35}
+        priority
+        /> :
+     <Image className='flag-border'
+     src={useFlagStore.getState().flag_geolocation}
+     alt={altFlagImage}
+     width={64}
+     height={35}
+     priority
+     />}
+    </div>
+
+
   )
 }
