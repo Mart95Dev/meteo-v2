004e6fd9f0dd0c423ed40db7607ba23485b6eb19
Mart95Dev <d.wehrling@gmail.com>
2024-07-31 17:08:37 +0200

    update useCountryStore

--- app/store/useCountryStore.tsx
+++ app/store/useCountryStore.tsx
@@ -4,12 +4,14 @@ interface countryState {
     geo_country: string;
     geo_country_code: string;
     geo_city: string;
+    geo_capital: string;
     search_country: string;
     search_country_code: string;
     search_city: string;
     setGeoCountry: (geo_country: string) => void;
     setGeoCountryCode: (geo_country_code: string) => void;
     setGeoCity: (geo_city: string) => void;
+    setGeoCapital: (geo_capital: string) => void;
     setSearchCountry: (search_country: string) => void;
     setSearchCountryCode: (search_country_code: string) => void;
     setSearchCity: (search_city: string) => void;
@@ -19,12 +21,14 @@ const useCountryStore = create<countryState>((set)=>({
     geo_country: "",
     geo_country_code: "",
     geo_city: "",
+    geo_capital: "",
     search_country: "",
     search_country_code: "",
     search_city: "",
     setGeoCountry: (geo_country) => set({geo_country}),
     setGeoCountryCode: (geo_country_code) => set({geo_country_code}),
     setGeoCity: (geo_city) => set({geo_city}),
+    setGeoCapital: (geo_capital) => set({geo_capital}),
     setSearchCountry: (search_country) => set({search_country}),
     setSearchCountryCode: (search_country_code) => set({search_country_code}),
     setSearchCity: (search_city) => set({search_city}),
