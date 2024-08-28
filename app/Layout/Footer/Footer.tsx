31f2cadc830f58f0b299efefe9712605fcc5a9ae
Mart95Dev <d.wehrling@gmail.com>
2024-08-28 10:41:09 +0200

    Update Footer component

--- app/Layout/Footer/Footer.tsx
+++ app/Layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Tous droits réservés - Réaliser par Martial Wehrling - Github - Contact </p>
+      <p>Tous droits réservés - Réaliser par Martial Wehrling - Contact </p>
      
     </footer>
   );
