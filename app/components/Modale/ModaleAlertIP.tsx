9715b994ac6bd12682177adfd2105372b01b5a16
Mart95Dev <d.wehrling@gmail.com>
2024-08-28 12:39:33 +0200

    update component ModaleAlertIp

--- app/components/Modale/ModaleAlertIP.tsx
+++ app/components/Modale/ModaleAlertIP.tsx
@@ -1,11 +1,13 @@
 import useErrorStore from "@/app/store/useErrorStore";
+import useGeolocationStore from "@/app/store/useGeolocationStore";
 
 export default function ModaleAlertIP() {
   const { error } = useErrorStore();
+  const {isGeolocationEnabled } = useGeolocationStore();
 
   return (
     
-      <div className="weather-box modale-alert">
+      <div className={isGeolocationEnabled ? "modale-alert-disabled":"modale-alert"}>
         <p className="message-alert">{error}</p>
         <button className="button-alert">Continuer</button>
       </div>
