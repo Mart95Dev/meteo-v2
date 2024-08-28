2f4b45c05f1f8f3820ad895e093af3056c939c82
Mart95Dev <d.wehrling@gmail.com>
2024-08-28 10:40:47 +0200

    update ModaleAlertIp component

--- app/components/Modale/ModaleAlertIP.tsx
+++ app/components/Modale/ModaleAlertIP.tsx
@@ -1,13 +1,14 @@
-import useErrorStore from "@/app/store/useErrorStore"
+import useErrorStore from "@/app/store/useErrorStore";
 
 export default function ModaleAlertIP() {
-const{error} = useErrorStore();
-
+  const { error } = useErrorStore();
 
   return (
-    <div className="weather-box modale-alert">
-      <p className="message-alert">{error}</p>
-      <button className="button-alert">Continuer</button>
+    
+      <div className="weather-box modale-alert">
+        <p className="message-alert">{error}</p>
+        <button className="button-alert">Continuer</button>
       </div>
-  )
+   
+  );
 }
