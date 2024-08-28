0599d910ddf3e3d607fa9a77bcdadf44a7469c99
Mart95Dev <d.wehrling@gmail.com>
2024-08-28 12:39:12 +0200

    add display conditionnal alert

--- app/Layout/Header/Header.tsx
+++ app/Layout/Header/Header.tsx
@@ -2,33 +2,26 @@ import { useEffect, useState } from "react";
 import HeaderImageLocation from "@/app/components/Geolocation/HeaderImageGeolocation";
 import useFetchHeaderGeolocationPhotoStore from "@/app/store/useFetchHeaderGeolocationPhoto";
 
+
 export default function Header() {
   const [isLoading, setIsLoading] = useState(true);
   const { geo_photo } = useFetchHeaderGeolocationPhotoStore();
 
- useEffect(() => {
-  if (geo_photo) {
-    setIsLoading(false);
-  }
- }, [geo_photo])
- 
+  useEffect(() => {
+    if (geo_photo) {
+      setIsLoading(false);
+    }
+  }, [geo_photo]);
 
   return (
     <header>
-           {isLoading ? (
+      {isLoading ? (
         <p>Recherche de la photo...</p>
       ) : (
-        <HeaderImageLocation itemImage={geo_photo} /> 
+        <HeaderImageLocation itemImage={geo_photo} />
       )}
-
     </header>
   );
 }
 
-//   return (
-//     <header>
-//       {geo_photo ? <HeaderImageLocation itemImage={geo_photo}/> : "Recherche en cours..."}
 
-//     </header>
-//   );
-// }
--- app/reusable/Title/Title.tsx
+++ app/reusable/Title/Title.tsx
@@ -1,12 +1,17 @@
+import useGeolocationStore from "@/app/store/useGeolocationStore";
+
+
 type ChildrenProps = {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
 };
 
 export default function Title({ level, children }: ChildrenProps) {
+  const {isGeolocationEnabled } = useGeolocationStore();
   const Tag = `h${level}` as keyof JSX.IntrinsicElements;
+  
   return (
-    <div className="box-title">
+    <div className={isGeolocationEnabled ? "box-title" : "box-title-disabled"}>
       <Tag className="title poppins-semibold">{children}</Tag>
     </div>
   );
