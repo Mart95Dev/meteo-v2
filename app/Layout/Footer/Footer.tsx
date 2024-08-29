8509515a11a13a41422e5f7c607057752f8f63a2
Mart95Dev <d.wehrling@gmail.com>
2024-08-29 19:29:40 +0200

    update styling

--- app/Layout/Footer/Footer.tsx
+++ app/Layout/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Tous droits réservés - Réaliser par Martial Wehrling - Contact </p>
+      <p>Tous droits réservés - Réalisé par Martial Wehrling - <a href="mailto:d.wehrling@gmail.com">d.wehrling@gmail.com</a> </p>
      
     </footer>
   );
