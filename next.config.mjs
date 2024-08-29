f11d2fe87007e68b8ba1722dddbd3209f64904e1
Mart95Dev <d.wehrling@gmail.com>
2024-08-29 18:26:39 +0200

    add webpack in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,35 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http', // Le protocole utilisé par l'API OpenWeatherMap
-            hostname: 'openweathermap.org', // Le nom de domaine de l'API OpenWeatherMap
-            port: '',
-            pathname: '/img/wn/**', // Le chemin d'accès aux icônes météo
-          },
-          {  // Nouvelle entrée pour Pixabay
-            protocol: 'https',
-            hostname: 'pixabay.com',
-          },
-        ],
-      },  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http", // Le protocole utilisé par l'API OpenWeatherMap
+        hostname: "openweathermap.org", // Le nom de domaine de l'API OpenWeatherMap
+        port: "",
+        pathname: "/img/wn/**", // Le chemin d'accès aux icônes météo
+      },
+      {
+        // Nouvelle entrée pour Pixabay
+        protocol: "https",
+        hostname: "pixabay.com",
+      },
+    ],
+  },
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new MiniCssExtractPlugin({
+          filename: "static/css/[name].[contenthash:8].css",
+          chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+        })
+      );
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
+// module.exports = nextConfig;
