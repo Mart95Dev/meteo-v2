57c0e1d4bcfe474564bb7b721532dea9287d5093
Mart95Dev <d.wehrling@gmail.com>
2024-08-30 12:21:26 +0200

    Update styling and function Aside component

--- app/Layout/Aside/Aside.tsx
+++ app/Layout/Aside/Aside.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import useGeolocationStore from "@/app/store/useGeolocationStore";
-
 
 interface AsideProps {
   isMobile: boolean;
@@ -10,10 +8,13 @@ interface AsideProps {
 
 export default function Aside({ isMobile }: AsideProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState(false);
   const { isGeolocationEnabled } = useGeolocationStore();
 
   const toggleAside = () => {
     setIsOpen(!isOpen);
+    setIsActive(true);
+    setTimeout(() => setIsActive(false), 300); // Réinitialise après 300ms
   };
 
   if (!isMobile) {
@@ -27,19 +28,19 @@ export default function Aside({ isMobile }: AsideProps) {
 
   return (
     <>
-    {isMobile && isGeolocationEnabled && (
-      <button 
-        className="aside-toggle" 
-        onClick={toggleAside}
-        aria-label={isOpen ? "Fermer le panneau latéral" : "Ouvrir le panneau latéral"}
-      >
-        {isOpen ? <FaChevronRight /> : <FaChevronLeft />}
-      </button>
-    )}
-    <aside className={`mobile ${isOpen ? 'open' : ''}`}>
-      <p>aside</p>
-      {/* Contenu de l'Aside en mode mobile */}
-    </aside>
-  </>
+      {isMobile && isGeolocationEnabled && (
+        <button 
+          className={`aside-toggle ${isActive ? 'active' : ''}`}
+          onClick={toggleAside}
+          aria-label={isOpen ? "Fermer le panneau latéral" : "Ouvrir le panneau latéral"}
+        >
+          {isOpen ? <FaChevronRight /> : <FaChevronLeft />}
+        </button>
+      )}
+      <aside className={`mobile ${isOpen ? 'open' : ''}`}>
+        <p>aside</p>
+        {/* Contenu de l'Aside en mode mobile */}
+      </aside>
+    </>
   );
 }
