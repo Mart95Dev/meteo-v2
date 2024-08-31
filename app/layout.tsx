3235710426d6e89279283b96d0123e390fc625ca
Mart95Dev <d.wehrling@gmail.com>
2024-08-31 13:50:19 +0200

    Add meta viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./styles/globals.css";
 export const metadata: Metadata = {
   title: "Weather APP",
   description: "Climat countries or cities informations",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
 };
 
 export default function RootLayout({
